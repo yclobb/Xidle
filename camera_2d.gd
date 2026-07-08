88e79f4e97b4a1e9db2867fbb3a8e845c449d03c
yclobb <lucas.liu8621@gmail.com>
2026-07-09 09:28:06 +1000

    camer updated code

--- camera_2d.gd
+++ camera_2d.gd
@@ -1,12 +1,21 @@
 extends Camera2D
 @export var cameraspeed: int = 300
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var inpvector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") 
+	var inpvector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	if Input.is_key_pressed(KEY_A):
+		inpvector.x -= 1.0
+	if Input.is_key_pressed(KEY_D):
+		inpvector.x += 1.0
+	if Input.is_key_pressed(KEY_W):
+		inpvector.y -= 1.0
+	if Input.is_key_pressed(KEY_S):
+		inpvector.y += 1.0
+
 	position += inpvector * cameraspeed * delta
 
